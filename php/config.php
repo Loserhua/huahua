bfb8d3a45e1758adb53ca2231cccf451de66380b
Loserhua <917140467@qq.com>
2016-06-22 21:38:44 +0800

    two
    aaa

--- php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = @mysqli_connect("localhost","root","13610428466","biye") or die('数据库链接失败：'.mysqli_connect_error());
-mysqli_select_db($conn,"biye") or die('数据库错误：'.mysqli_connect_error());
-mysqli_query($conn,"set names 'UTF8'");
-
-
-?>
